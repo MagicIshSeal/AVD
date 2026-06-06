628ba1fbd7fb6b44716c9b91c56d1101bde22e97
Max van Eikeren <maxvaneikeren@gmail.com>
2026-06-06 20:50:59 +0200

    commit

--- 34.py
+++ 34.py
@@ -5,16 +5,24 @@ import pandas as pd
 def dydx(y1, y2, x1, x2):
     return (y2-y1)/(x2-x1)
 
-cd = pd.read_csv('.\\csv\\Cd.csv')
-cl = pd.read_csv('.\\csv\\Cl.csv')
-cm = pd.read_csv('.\\csv\\Cm.csv')
+ref = pd.read_csv('morecsv/reference.csv', skiprows=5)
+closer = pd.read_csv('morecsv/longer_tail_chord.csv', skiprows=5)
 
-dfs = {"cd": cd, "cl": cl, "cm": cm}
+cm = pd.read_csv('Tain/Cm_a_2.csv')
+
+print(cm.head())
+cm.drop(' Alpha.1')
+cm.plot()
+print(cm.head())
+plt.show()
+
+dfs = {"ref": ref, "Bigger Chord": closer}
 
 for df_name, df in dfs.items():
-    df['rad'] = np.radians(df['Alpha'])
-    df_alpha = dydx(df['T1-50.0 m/s-VLM1+Drag'].iloc[0], df['T1-50.0 m/s-VLM1+Drag'].iloc[-1], df['rad'].iloc[0], df['rad'].iloc[-1])
-    print(f"{df_name}_0: {df['T1-50.0 m/s-VLM1+Drag'].iloc[0]}")
-    print(f"{df_name}_alpha: {df_alpha}\n")
-    df.plot(x='rad', y='T1-50.0 m/s-VLM1+Drag', label=df_name)
-    plt.show()
+    df['rad'] = np.radians(df['alpha'])
+    dCddA = dydx(df[' CD'].iloc[0], df[' CD'].iloc[-1], df['rad'].iloc[0], df['rad'].iloc[-1])
+    dCldA = dydx(df[' CL'].iloc[0], df[' CL'].iloc[-1], df['rad'].iloc[0], df['rad'].iloc[-1])
+    dCmdA = dydx(df[' Cm'].iloc[0], df[' Cm'].iloc[-1], df['rad'].iloc[0], df['rad'].iloc[-1])
+    print(f"{df_name}: Cd_a = {dCddA:2f}, Cl_a = {dCldA:2f}, Cm_a = {dCmdA:2f}")
+    idx = abs(df['alpha']).idxmin() 
+    print(f"{df_name}: Cd_0 = {df[' CD'].iloc[idx]:2f}, Cl_0 = {df[' CL'].iloc[idx]:2f}, Cm_0 = {df[' Cm'].iloc[idx]:2f}")
